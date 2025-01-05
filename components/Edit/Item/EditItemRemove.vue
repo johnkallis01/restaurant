c6aab229c02ea3cb47da30526e20d244a196a05c
John Kallis <johnkallis01@gmail.com>
2025-01-05 11:12:51 -0500

    branch

--- components/Edit/Item/EditItemRemove.vue
+++ components/Edit/Item/EditItemRemove.vue
@@ -3,14 +3,12 @@ const props = defineProps({
     removes: { type: Array, required: false },
     item_id: { type: String, required: true },
     section_id: { type:String, required: true},
-    menu: { type:Object, required: true},
-    
+    menu: { type:Object, required: true},   
 })
 const viewRemoves = ref(false);
 </script>
 <template>
     <div class="remove-container">
-        <span class="title-text" @click="viewRemoves=!viewRemoves">Removes:</span>
         <div class="remove" v-if="viewRemoves">
             <p class="remove-name">removes go here</p>
         </div>
