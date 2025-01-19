b52aa16d747e3e0472bf6d5ab30458e3d990fc80
John Kallis <johnkallis01@gmail.com>
2025-01-19 12:49:08 -0500

    editmenutimes dynamic

--- components/DisplaySchedule.vue
+++ components/DisplaySchedule.vue
@@ -1,32 +1,5 @@
 <script setup>
-const props=defineProps({menu:{type: Object, required: false}});
-const openDays=ref(props.menu.days.filter(day => day.open));
-function similarDays(){
-    console.log(props.menu.name)
-    console.log(openDays.value.length)
-    let similarDays = [];
-    let redoLoop=[];
-    let previousDay;
-    if(openDays.value.length){
-        previousDay=openDays.value[0];
-    }
-    
-    for(let i=1; i< openDays.value.length;i++){
-        if(
-            previousDay.start.hour===openDays.value[i].start.hour &&
-            previousDay.end.hour===openDays.value[i].end.hour &&
-            previousDay.start.min===openDays.value[i].start.min &&
-            previousDay.end.min===openDays.value[i].end.min
-        ) {
-            similarDays.push(openDays.value[i])
-            previousDay=openDays.value[i]
-        }else{
-            redoLoop.push(openDays.value[i])
-        }
-    }
-    console.log('similar',similarDays)
-    console.log('not', redoLoop)
-}
+
 </script>
 <template>
     <div class="schedule">
